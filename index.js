d4eccd7f64248a157c387d8137bf61d28e05b923
DonTrevi <valentin.trevi@gmail.com>
2021-04-14 23:27:30 -0300

    Boton darkmode añadido

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //alert('its working')
+
+// NAV BAR LOGIC!
 let menuToggle = document.querySelector('.menu-toggle');
-let menuToggleIcon = document.querySelector('.material-icons-outlined');
+let menuToggleIcon = document.querySelector('#burguer_menu');
 let menu = document.querySelector('#menu');
 
-console.log(menuToggleIcon)
+
 
 menuToggle.addEventListener('click', e=>{
   menu.classList.toggle('show');
@@ -13,3 +15,28 @@ menuToggle.addEventListener('click', e=>{
     menuToggleIcon.innerHTML = 'view_headline'
   }
 })
+
+//DARKMODE LOGIC:
+
+const btnSwitch = document.querySelector('#bdark');
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('darkmode');
+  btnSwitch.classList.toggle('active');
+
+  //Save config in localStorage:
+  if(document.body.classList.contains('darkmode')){
+    localStorage.setItem('darkmode', 'true');
+  }else{
+    localStorage.setItem('darkmode', 'false');
+  }
+});
+
+  //Ask for actual mode:
+  if(localStorage.getItem('darkmode') == 'true') {
+    document.body.classList.add('darkmode')
+    btnSwitch.classList.add('active');
+  }else{
+    document.body.classList.remove('darkmode')
+    btnSwitch.classList.remove('active');
+  }
